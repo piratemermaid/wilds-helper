b93098fe1ed056d9ec971c030518acf067d4e18c
Ariel <thepiratemermaid4@gmail.com>
2025-03-22 11:28:44 -0600

    add accent color to theme

--- src/themes/dark.ts
+++ src/themes/dark.ts
@@ -1,9 +1,16 @@
 import { createTheme } from '@mui/material';
-import { blueGrey, green, grey, lightBlue } from '@mui/material/colors';
+import {
+  blueGrey,
+  deepOrange,
+  green,
+  grey,
+  lightBlue,
+} from '@mui/material/colors';
 
 const palette = {
   primary: lightBlue[500],
   secondary: green[400],
+  accent: deepOrange[300],
   disabled: grey[500],
   background: blueGrey[900],
   gray: grey[300],
@@ -17,6 +24,9 @@ export const darkTheme = createTheme({
     },
     secondary: {
       main: palette.secondary,
+    },
+    accent: {
+      main: palette.accent,
     },
     action: {
       disabled: palette.disabled,
